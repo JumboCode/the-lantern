a492fd77de5776cb1d83c2da6861432737217a69
tlai18 <thomas.lai@tufts.edu>
2023-10-15 18:58:59 -0400

    initial prep

--- lantern-club/src/components/Navbar.js
+++ lantern-club/src/components/Navbar.js
@@ -5,9 +5,7 @@ export default function NavBar() {
         <div>
             <ul className="flex space-x-8">
                 <li><Link href="/" className="hover:underline">Home</Link></li>
-                <li><Link href="/profile" className="hover:underline">Profile</Link></li>
-                <li><Link href="/magazine" className="hover:underline">Magazine</Link></li>
-                <li><Link href="/events" className="hover:underline">Events</Link></li>
+                <li><Link href="/auth" className="hover:underline">Auth</Link></li>
             </ul>
         </div>
     )
--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -2,7 +2,8 @@ import NavBar from '../components/Navbar'
 export default function Home() {
   return (
     <div>
-      <header className="flex h-screen justify-center items-center">
+      <header className="flex flex-col h-screen justify-center items-center">
+          <p>Hi Lantern Club!! This is the default page aka homepage of the website</p>
           <NavBar/>
       </header>
     </div>
