3ffc5462bfcd737b7704132fe414a2e6e051ccd1
zoeguu <zoegu2003@gmail.com>
2023-10-27 14:07:11 -0400

    set up basic api route

---
+++ lantern-club/src/pages/api/content/magazine.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    // Process a GET request
+  } else {
+    // Handle any other HTTP method
+  }
+}
