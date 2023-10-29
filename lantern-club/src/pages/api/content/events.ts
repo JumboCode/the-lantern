59c7d6fb91ce4bb3abedcbf2e0de12eb6672925b
zoeguu <zoegu2003@gmail.com>
2023-10-29 19:52:22 -0400

    JSON response

--- lantern-club/src/pages/api/content/events.ts
+++ lantern-club/src/pages/api/content/events.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     // Process a GET request
+    console.log('events: GET requested received');
   } else {
     // Handle any other HTTP method
   }
