ce275ff2c6da89a35093e618595d946ee7119e6a
Fahim Rashid <fahim@Fahims-MacBook-Air.local>
2023-10-29 19:19:29 -0400

    for arjun to see

---
+++ lantern-club/src/components/Header.tsx
@@ -0,0 +1,8 @@
+import {useState} from "react";
+import React from "react";
+
+export default function Header({title}:{title:string}){
+
+
+    return()
+}
--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import NavBar from '../components/Navbar'
+import Header from '../components/Header'
 export default function Home() {
   return (
     <div>
       <header className="flex flex-col h-screen justify-center items-center">
           <p>Hi Lantern Club!! This is the default page aka homepage of the website</p>
+          <Header title="The Lantern"/>
           <NavBar/>
       </header>
     </div>
