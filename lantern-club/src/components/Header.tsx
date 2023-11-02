46aa035e1388b53246056a1a23d6b9f1f5a102f8
Fahim Rashid <fahim@Fahims-MacBook-Air.local>
2023-11-01 22:42:54 -0400

    I added a gradient and a header with the font while messing around with the code, but are we supposed to add background color for the entire thing?

--- lantern-club/src/components/Header.tsx
+++ lantern-club/src/components/Header.tsx
@@ -1,8 +1,13 @@
-import {useState} from "react";
-import React from "react";
+import React  from 'react'
+import logo from '../images/logo.png';
 
 export default function Header({title}:{title:string}){
-
-
-    return()
+     return(
+        <div>
+            <div className="flex flex-row bg-gradient-to-r from-sky-600 to-purple-900">
+            <img src={logo.src} className="h-16 mx-3.5"/>
+            <h1 className="text-5xl font-kiona hover:font-serif hover:rotate-90">The Lantern Club</h1>
+            </div>
+        </div>
+     )
 }
--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -1,12 +1,12 @@
+import React from 'react'
 import NavBar from '../components/Navbar'
 import Header from '../components/Header'
 export default function Home() {
   return (
     <div>
       <header className="flex flex-col h-screen justify-center items-center">
-          <p>Hi Lantern Club!! This is the default page aka homepage of the website</p>
+          {/* <NavBar/> */}
           <Header title="The Lantern"/>
-          <NavBar/>
       </header>
     </div>
   )
