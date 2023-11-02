313b013a693b79e82e1b5f3f137df04dc1614790
Fahim Rashid <fahim@Fahims-MacBook-Air.local>
2023-11-01 22:47:03 -0400

    I added a gradient let me know what I can do next

--- lantern-club/src/components/Header.tsx
+++ lantern-club/src/components/Header.tsx
@@ -4,9 +4,9 @@ import logo from '../images/logo.png';
 export default function Header({title}:{title:string}){
      return(
         <div>
-            <div className="flex flex-row bg-gradient-to-r from-sky-600 to-purple-900">
+            <div className="flex flex-row">
             <img src={logo.src} className="h-16 mx-3.5"/>
-            <h1 className="text-5xl font-kiona hover:font-serif hover:rotate-90">The Lantern Club</h1>
+            <h1 className="text-5xl font-kiona">The Lantern Club</h1>
             </div>
         </div>
      )
--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -3,7 +3,7 @@ import NavBar from '../components/Navbar'
 import Header from '../components/Header'
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-sky-400 to-blue-800">
       <header className="flex flex-col h-screen justify-center items-center">
           {/* <NavBar/> */}
           <Header title="The Lantern"/>
