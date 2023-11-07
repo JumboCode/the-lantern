ac21230a5508d02e46b086cbdee04aff4802e212
21hwang <hannahwang714@gmail.com>
2023-11-06 20:48:19 -0500

    main push

--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -1,13 +1,19 @@
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <header className="flex flex-col h-screen justify-center items-center">
-        <p>
+      <header className="flex flex-col h-screen items-center">
+        <NavBar />{" "}
+        <p className="flex flex-col h-screen justify-center items-center">
           Hi Lantern Club!! This is the default page aka homepage of the website
+          <Link href="/auth" className="hover:underline">
+            Auth
+          </Link>
         </p>
-        <NavBar />
       </header>
 
       <footer className="flex items-center">
