90963b1ccf35917ba27552014fc3c1fee0e0cd37
zoeguu <zoegu2003@gmail.com>
2023-11-17 15:40:34 -0500

    Revert "event button error"
    This reverts commit e03dbb5fbd6e4b369c7f2bd28290f48df80b8a8e.

--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
-import EventsButton from "../components/events-button";
-import MyButton from "../components/Button";
 
 import Link from "next/link";
 
@@ -10,8 +8,6 @@ export default function Home() {
     <div>
       <header className="flex flex-col h-screen items-center">
         <NavBar />{" "}
-        <events-button label="EVENTS"/>
-        {/* <MyButton label="Button"/> */}
         <p className="flex flex-col h-screen justify-center items-center">
           Hi Lantern Club!! This is the default page aka homepage of the website
           <Link href="/auth" className="hover:underline">
@@ -19,11 +15,6 @@ export default function Home() {
           </Link>
         </p>
       </header>
-
-      <section>
-        <events-button label="EVENTS"/>
-      </section>
-
 
       <footer className="flex items-center">
         <Footer showAdminLogin={true} />
