72985328b9a3063938890a906597234af5646756
zoeguu <zoegu2003@gmail.com>
2023-11-30 16:39:20 -0500

    bring back error

---
+++ lantern-club/src/components/events-button.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+// import FetchButton from './FetchButton';
+
+export default function EventsButton ( { label } ) {
+    const buttonStyle = 
+        { color: 'red',
+            border: '2px solid red',
+            border_radius: '20px',
+            padding: '10px 20px',
+            align: 'center'
+        }
+    const divStyle = 
+        { display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }       
+
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState("");
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('/api/content/events'); 
+            const data = await response.json();
+            console.log('Fetched data:', data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+  return (
+//     <div>
+//       <h1>Data Fetching Component</h1>
+//       <FetchButton fetchData={fetchData} />
+//     </div>
+        <div style={divStyle}>
+                <button fetchData={fetchData} style={buttonStyle}>{label}</button>
+                result && <div>{result}</div>
+        </div>
+        // <div className="flex flex-col h-screen items-center">
+        //         <button onClick={fetchData}>EventsButton</button>
+        // </div>
+  );
+};
--- lantern-club/src/pages/index.tsx
+++ lantern-club/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
+import EventsButton from "../components/events-button";
+import MyButton from "../components/Button";
 
 import Link from "next/link";
 
@@ -8,6 +10,8 @@ export default function Home() {
     <div>
       <header className="flex flex-col h-screen items-center">
         <NavBar />{" "}
+        <events-button label="EVENTS"/>
+        {/* <MyButton label="Button"/> */}
         <p className="flex flex-col h-screen justify-center items-center">
           Hi Lantern Club!! This is the default page aka homepage of the website
           <Link href="/auth" className="hover:underline">
@@ -15,6 +19,11 @@ export default function Home() {
           </Link>
         </p>
       </header>
+
+      <section>
+        <events-button label="EVENTS"/>
+      </section>
+
 
       <footer className="flex items-center">
         <Footer showAdminLogin={true} />
