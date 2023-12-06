66fb231a794d030692c05af5737de3e4087a39fc
Fahim Rashid <fahim@Fahims-MacBook-Air.local>
2023-12-06 13:46:22 -0500

    Worked on the changes

--- lantern-club/src/components/Header.tsx
+++ lantern-club/src/components/Header.tsx
@@ -1,15 +1,16 @@
 import React  from 'react'
 import logo from '../images/logo.png';
 import NavBar from './Navbar';
+import lantern from '../images/lantern.png';
 
 export default function Header({title}:{title:string}){
      return(
         <div>
             <NavBar/>
-            <header className="flex h-13 bg-gradient-to-tr from-lanternblue from-40% to-lanternyellow to-80%">
+            <header className="flex flex-row h-13 bg-gradient-to-tr from-lanternblue from-40% to-lanternyellow to-80%">
                 <div className="flex flex-row m-14">
-                    <img src={logo.src} className="h-16 mx-3.5"/>
-                    <h1 className="text-5xl font-kiona text-right">The Lantern Club</h1>
+                    <h1 className="text-7xl font-kiona text-right">The Lantern</h1>
+                    <img src={lantern.src} className="container justify-end h-60 w-auto flex flex-row"/>
                  </div>
             </header>
         </div>
