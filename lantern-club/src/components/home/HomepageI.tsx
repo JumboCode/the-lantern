c329971ac17a58d355e974120ac7aa5f64ceb534
tlai18 <thomas.lai@tufts.edu>
2024-01-21 16:36:11 -0500

    responsive edits

--- lantern-club/src/components/home/HomepageI.tsx
+++ lantern-club/src/components/home/HomepageI.tsx
@@ -13,7 +13,7 @@ export default function HomepageI({title}:{title:string}){
                 {/* <div className="flex flex-row m-14"> */}
                     <img className="justify-self-end mr-20" src={lantern.src}/>
                     <h1 className="ml-20 pt-20 justify-self-start text-6xl md:text-8xl font-kiona text-right">The Lantern</h1>
-                    <h1 className="justify-self-end mr-20 mt-40 text-3xl md:text-4xl font-nunitolight text-center tracking-wide">A think-tank advocating socially responsible
+                    <h1 className="justify-self-end mr-20 mt-40 text-2xl md:text-4xl font-nunitolight text-center tracking-wide">A think-tank advocating socially responsible
                     <br></br> technology use and production</h1>
                     <img src={logo.src} height={200} width={200} className="flex inline-flex rotate-6 opacity-40 self-end overflow-x-hidden"/>
             </div>            
--- lantern-club/src/components/home/HomepageIV.tsx
+++ lantern-club/src/components/home/HomepageIV.tsx
@@ -3,56 +3,41 @@ import Image from "next/image";
 import LanternHead from "../../images/lantern-head.png";
 
 export default function HomepageIV() {
-  const header_font: React.CSSProperties = {
+  const headerFontStyle = {
     fontFamily: "coolvetica",
-    fontSize: "90px",
-    lineHeight: "1",
+    fontSize: "4rem",
+    lineHeight: "1.2", 
   };
-  
-  const image: React.CSSProperties = {
-    minHeight: "280px",
-    minWidth: "280px",
-  };
-  
+
   return (
-    <div
-        className="-mt-20 pb-20 pt-20 md:pt-0 w-full blue2-gradient flex grid grids-cols-1 justify-center md:grid-cols-2"
-      >
-        {/* image column div */}
-        <div className="flex px-20 md:pl-44 md:pt-32">
-          <div className="mt-6">
+    <div className="w-full blue2-gradient flex flex-col md:grid md:grid-cols-2 -mt-20 pb-20 pt-20 md:pt-0">
+      {/* Image Column */}
+      <div className="flex justify-center px-4 md:px-20 md:pt-32">
+        <div className="mt-6">
           <Image
             src={LanternHead}
             height={280}
             width={280}
-            style={image}
             alt="LanternHead picture"
           />
+        </div>
+      </div>
+
+      {/* Text Column */}
+      <div className="px-4 md:px-20 md:mt-32 text-center md:text-left">
+        <div className="mt-5">
+          <p style={headerFontStyle}>Who are we?</p>
+          <div className="pt-5 font-nunito text-xl">
+            <p>We are a student-led think tank that investigates the societal impacts of modern technology. We want to break down complex ideas at the intersection of science, technology, and society (STS) through student-led discussions and research.</p>
+          </div>
+          <div className="mt-5">
+            <a href="/AboutUs">
+              <button className="font-nunitosans tracking-wide bg-button-blue text-white font-bold py-2 px-9 rounded-full border-2 border-borderblue"> 
+              Learn More</button>
+            </a>
           </div>
         </div>
-        {/* text column div */}
-        <div className="md:ml-10 md:mt-32 ">
-            <div className="mt-5">
-            <p style={header_font}>Who are we?</p>
-                <div className="pt-5 font-nunito text-xl">
-                <p>We are a student led think tank that investigates the</p>
-                <p>societal impacts of modern technology. We want to break </p>
-                <p>down complex ideas at the intersection of science, </p>
-                <p>technology, and society (STS) through student led </p>
-                <p>discussions and research.</p>
-                </div>
-                <div className="justify-self-center pt-1">
-                    <div className="mt-5">
-                    <a href="/AboutUs">
-                        <button className="font-nunitosans tracking-wide bg-button-blue text-white font-bold py-2 px-9 rounded-full border-2 border-borderblue"> 
-                        Learn More</button>
-                    </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        
+      </div>
     </div>
   );
 }
