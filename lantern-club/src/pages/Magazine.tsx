1853ed4859888346cc5c6a69700c852104f547bf
jonchung1205 <jon.chung1205@gmail.com>
2024-01-28 19:47:45 -0500

    cloud

--- lantern-club/src/pages/Magazine.tsx
+++ lantern-club/src/pages/Magazine.tsx
@@ -1,12 +1,13 @@
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
-
+import FileUpload from "../components/FileUpload";
 export default function Magazine() {
   return (
     <>
       <NavBar />
       <p>MAGAZINE PAGE!</p>
       <Footer showAdminLogin={true} />
+      <FileUpload /> 
     </>
   );
 }
