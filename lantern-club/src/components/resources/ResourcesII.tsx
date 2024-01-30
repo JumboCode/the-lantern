25aff6c79cf97f1dcca0faa8b26330c2cd2419ec
zoeguu <zoegu2003@gmail.com>
2024-01-30 18:43:03 -0500

    orange text box problems

--- lantern-club/src/components/resources/ResourcesII.tsx
+++ lantern-club/src/components/resources/ResourcesII.tsx
@@ -40,14 +40,14 @@ export default function ResourcesI() {
 
   const round_box: React.CSSProperties = {
     display: "flex",
-    gap: "20px",
+    gap: "10px",
     borderWidth: "2px",
     borderRadius: "1.5rem",
-    width: "1000px",
-    height: "200px",
+    // width: "1000px",
+    // height: "200px",
     padding: "5rem",
     filter: "drop-shadow(5px 10px 3px rgb(0 0 0 / 0.2)) drop-shadow(0 8px 5px rgb(0 0 0 / 0.08))",
-    margin: "0 auto"
+    // margin: "0 auto"
   };
 
   return (
@@ -56,11 +56,11 @@ export default function ResourcesI() {
         className="-mt-20 py-40 px-20 yellow-gradient" style={background}> 
         {/* text side */}
         <div className="py-20 px-4 md:px-0 md:ml-10">
-            <p className="mb-20" style={headerFontStyle}>
+            <p className="mb-10" style={headerFontStyle}>
                 Our e-List
             </p>
-            <div className="orange mellow-yellow-border" style={round_box}>
-              <div className="flex py-3 font-nunito text-xl items-center">
+            <div className="flex-col md:flex-row md:grid md:grid-cols-2 orange mellow-yellow-border font-nunito text-xl" style={round_box}>
+            <div className="flex flex-col justify-center items-start px-5">
               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
             aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
