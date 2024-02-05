e258e88e314c7e0c0e1e3b85c4ad57a50690e0be
tlai18 <thomas.lai@tufts.edu>
2024-02-05 13:00:05 -0500

    responsive header

--- lantern-club/src/components/Header.tsx
+++ lantern-club/src/components/Header.tsx
@@ -9,12 +9,12 @@ export default function Header({title}:{title:string}){
     return(
         <div className="drop-shadow-md">
                 <div className="flex items-start justify-center bg-center bg-cover bg-no-repeat bg-[url('../images/header-bg.png')]">
-                        <div>
-                        <Image src={lantern} height={90} width={90} alt="lantern logo" />
+                        <div className='flex flex-shrink-0'>
+                                <Image src={lantern} height={90} width={90} alt="lantern logo" />
                         </div>
-                        <h1 className="ml-16 mr-16 pt-24 text-8xl font-kiona text-center">{title}</h1>
-                        <div>
-                        <Image src={lantern} height={90} width={90} alt="lantern logo" />
+                        <h1 className="flex text-4xl flex-shrink md:ml-16 md:mr-16 pt-24 md:text-8xl font-kiona text-center">{title}</h1>
+                        <div className='flex flex-shrink-0'>
+                                <Image src={lantern} height={90} width={90}  alt="lantern logo" />
                         </div>
                 </div>            
                 <div className="h-20 bg-bottom bg-cover bg-no-repeat bg-[url('../images/header-bg.png')]" id="triangle"></div>
