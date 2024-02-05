e43cec81eae39c00e3c241887e0bd93a3193720b
hallehau <hallejhau@gmail.com>
2024-02-04 20:41:14 -0500

    Contact form submit functionality WIP

--- lantern-club/src/components/Contact.tsx
+++ lantern-club/src/components/Contact.tsx
@@ -1,38 +1,73 @@
 import React from "react"
+import { FormEvent } from 'react'
+import { render } from '@react-email/render';
+import nodemailer from 'nodemailer';
+// import { Email } from './email';
 
 export default function Contact() {
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('/api/content/contact', {
+      method: 'POST',
+      body: formData,
+    })
 
+    // Handle response if necessary
+    const data = await response.json()
+    console.log("Submit button pressed!")
+}
     return (
-        <div className="flex flex-col bg-gradient-to-b from-blue-g1 to-blue-g2">
+ <div className="flex flex-col bg-gradient-to-b from-blue-g1 to-blue-g2">
  <div className="h-20 w-full flex-1 mellow-yellow" id="triangle"> 
 
-whathelome
+</div>
+    <h1 className="mt-20 font-coolvetica text-7xl ml-12">Get in touch!</h1>
 
-</div> 
-            <h1 className="mt-20 font-coolvetica text-7xl ml-12">Get in touch!</h1>
+        {/* <div className="flex flex-col border-contact-g1 border-4 mx-16 mb-16 mt-10 px-7 h-contact bg-gradient-to-b from-contact-g1 to-contact-g2 rounded-3xl">
+        <div>
+        <h2 className="mt-5 text-nunito text-lg">Name</h2>
+        <input className="mt-2 p-3 border-contact-g1 border-4 rounded-full outline-gc2 h-8 contact-input" type="text"></input>
+        </div>
+
+        <div>
+        <h2 className="mt-5 text-nunito text-lg">Email</h2>
+        <input className="mt-2 p-3 border-contact-g1 border-4 rounded-full outline-gc2 h-8 contact-input" type="text"></input>
+        </div> */}
+
+        {/* <div>
+        <h2 className="mt-5 text-nunito text-lg">Message</h2>
+        <textarea className="mt-2 w-full p-3  border-contact-g1 border-4 mr-7 contact-input-height rounded-3xl"></textarea>
+        <br></br>
+        </div> */}
         
-              <div className="flex flex-col border-contact-g1 border-4 mx-16 mb-16 mt-10 px-7 h-contact bg-gradient-to-b from-contact-g1 to-contact-g2 rounded-3xl">
+        {/* Only issue here is that I am making the button manually move down verticall I will figure out how to do that */}
+        {/* <div className="flex justify-center items-center"> */}
+
+        <form onSubmit={onSubmit}>
+            <div className="flex flex-col border-contact-g1 border-4 mx-16 mb-16 mt-10 px-7 h-contact bg-gradient-to-b from-contact-g1 to-contact-g2 rounded-3xl">
                 <div>
-                <h2 className="mt-5 text-nunito text-lg">Name</h2>
-                <input className="mt-2 p-3 border-contact-g1 border-4 rounded-full outline-gc2 h-8 contact-input" type="text"></input>
+                    <h2 className="mt-5 text-nunito text-lg">Name</h2>
+                    <input className="mt-2 w-full p-3 border-contact-g1 border-4 mr-7 rounded-full outline-gc2 h-8 contact-input rounded-3xl" type="text"></input>
                 </div>
 
-                <div> 
-                <h2 className="mt-5 text-nunito text-lg">Email</h2>
-                <input className="mt-2 p-3 border-contact-g1 border-4 rounded-full outline-gc2 h-8 contact-input" type="text"></input>
+                <div>
+                    <h2 className="mt-5 text-nunito text-lg">Email</h2>
+                    <input className="mt-2 w-full p-3 border-contact-g1 border-4 mr-7 rounded-full outline-gc2 h-8 contact-input rounded-3xl" type="text"></input>
+                </div>
+            
+                <div>
+                    <h2 className="mt-5 text-nunito text-lg">Message</h2>
+                    <textarea className="mt-2 w-full p-3  border-contact-g1 border-4 mr-7 contact-input-height rounded-3xl"></textarea>
+                    <br></br>
                 </div>
 
-                <div>
-                <h2 className="mt-5 text-nunito text-lg">Message</h2>
-                <textarea className="mt-2 w-full p-3  border-contact-g1 border-4 mr-7 contact-input-height rounded-3xl"></textarea>
-                <br></br>
-                </div>
-                
-                {/* Only issue here is that I am making the button manually move down verticall I will figure out how to do that */}
                 <div className="flex justify-center items-center">
-                <button className="bg-button-blue border-button-outline-blue border-2 mt-9 text-white font-bold py-2 px-10 mb-8 rounded-3xl" type="submit">Submit</button>      
+                    <button className="bg-button-blue border-button-outline-blue border-2 mt-9 text-white font-bold py-2 px-10 mb-8 rounded-3xl" type="submit">Submit</button> 
                 </div>
             </div>
+        </form>
         </div>
     )
 }
--- lantern-club/src/pages/api/content/contact.ts
+++ lantern-club/src/pages/api/content/contact.ts
@@ -1,14 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {handlePostProfile, insertEvent, insertProfile} from '../../../../prisma/insert-data';
+import { send } from 'process';
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    // Process a GET request
-    insertProfile()
-    const responseData = { message: 'profile: GET requested received' };
-    res.status(200).json(responseData);
-  } else if (req.method === 'POST') {
+  if (req.method === 'POST') {
     // Process a POST request
-    handlePostProfile(req, res)
+    // handlePostProfile(req, res);
     const responseData = { message: 'profile: POST requested received' };
     res.status(200).json(responseData);
   } else {
