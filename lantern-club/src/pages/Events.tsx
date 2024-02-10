9a1f2c2c58425f7ee7dd571baeaf1e2cb65ebd09
tlai18 <thomas.lai@tufts.edu>
2024-02-10 14:24:22 -0500

    added Upcomming Events which was already done by another team member

--- lantern-club/src/pages/Events.tsx
+++ lantern-club/src/pages/Events.tsx
@@ -41,6 +41,18 @@ export default function Events() {
       <NavBar />
       <Header title="Events"/>
         {/* <div> */}
+        <div className="-mt-20 flex flex-col h-contact bg-gradient-to-t from-contact-g2 to-g-yellow1">
+            <h1 className="mt-20 font-coolvetica text-7xl ml-12">Upcomming Events</h1>
+            <div className="flex flex-row">
+                {/* <div className="px-eboardx py-eboardy bg-lanternblue">
+
+                </div> */}
+
+            </div>
+
+        </div>
+        <div className="flex flex-col bg-gradient-to-b from-blue-g1 to-blue-g2"></div>
+        <div className="h-20 w-full flex-1 mellow-yellow" id="triangle"></div>
 
        {AllEvents && AllEvents.map((oneEvent: event) => {
         return (
