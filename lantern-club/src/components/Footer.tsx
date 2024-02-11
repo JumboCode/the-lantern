9b49a19e40de17795ceea71c2cf4d4562553ef44
tlai18 <thomas.lai@tufts.edu>
2024-02-11 15:24:27 -0500

    footer buttons responsive and small tweaks on the footer fonts

--- lantern-club/src/components/Footer.tsx
+++ lantern-club/src/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer({ showAdminLogin }: { showAdminLogin: boolean }) 
               };
 
   return (
-    <div className="font-nunito bg-[#d5e1f3] p-10 w-full text-[#4279bc] flex items-center justify-center">
+    <div className="bg-[#d5e1f3] p-10 w-full text-[#4279bc] flex items-center justify-center">
       <div className="grid w-full max-w-screen-xl grid-cols-1 md:grid-cols-3">
         {/* Image column */}
         <div className="flex flex-col md:flex-row items-center md:items-start">
@@ -24,21 +24,21 @@ export default function Footer({ showAdminLogin }: { showAdminLogin: boolean }) 
               <Image src={Logo} layout="fill" objectFit="contain" alt="Lantern Club Logo" />
             </Link>
           </div>
-          <div className="mt-4 md:mt-0 md:ml-8 flex flex-col">
+          <div className="font-coolvetica items-center md:items-start mt-4 md:mt-0 md:ml-8 flex flex-col">
             {/* Links here */}
-            <Link href="/Magazine" className="py-1 text-xl hover:underline">
+            <Link href="/Magazine" className="py-1 text-2xl hover:underline">
               Magazine
             </Link>
-            <Link href="/Events" className="py-1 text-xl hover:underline">
+            <Link href="/Events" className="py-1 text-2xl hover:underline">
               Events
             </Link>
-            <Link href="/AboutUs" className="py-1 text-xl hover:underline">
+            <Link href="/AboutUs" className="py-1 text-2xl hover:underline">
               About Us
             </Link>
-            <Link href="/ContactUs" className="py-1 text-xl hover:underline">
+            <Link href="/ContactUs" className="py-1 text-2xl hover:underline">
               Contact Us
             </Link>
-            <Link href="/Resources" className="py-1 text-xl hover:underline">
+            <Link href="/Resources" className="py-1 text-2xl hover:underline">
               Resources
             </Link>
           </div>
@@ -52,7 +52,7 @@ export default function Footer({ showAdminLogin }: { showAdminLogin: boolean }) 
         {/* Login/Submit column */}
         <div className="flex flex-col md:flex-row items-center md:items-end justify-end p-4">
           {/* Login and Submit buttons here */}
-          <div className="font-nunitosans flex flex-col pr-10 space-y-4 items-start justify-start">
+          <div className="font-nunitosans flex flex-col pb-5 md:pb-0 pr-0 md:pr-10 space-y-4 items-start justify-start">
             {showAdminLogin && (
         //       <button
         //         className=" border-[#94BBE3] bg-[#4279BC] border-2 text-white text-lg rounded-full w-36 h-11 hover:underline flex items-center justify-center"
@@ -60,7 +60,7 @@ export default function Footer({ showAdminLogin }: { showAdminLogin: boolean }) 
         //       >
         //         Login
         //       </button>
-              <Buttonv2 text="Admin Login" action={() => signIn("google")} color="blue" width="w-40" />
+              <Buttonv2 text="Admin Login" action={() => signIn("google")} color="blue" width="w-48" />
 
             )}
             {/* <button
@@ -68,7 +68,7 @@ export default function Footer({ showAdminLogin }: { showAdminLogin: boolean }) 
             >
               Submit
             </button> */}
-            <Buttonv2 text="Submit Work" action={handleButtonClick} color="orange" width="w-40" />
+            <Buttonv2 text="Submit Work" action={handleButtonClick} color="orange" width="w-48" />
 
           </div>
 
