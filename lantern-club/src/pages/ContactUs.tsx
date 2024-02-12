85b62637953c8f6fa8d0e5d7e9c71aa6461fb7d6
tlai18 <thomas.lai@tufts.edu>
2024-02-11 23:14:43 -0500

    move components to contact folder and also made card component to its own folder

--- lantern-club/src/pages/ContactUs.tsx
+++ lantern-club/src/pages/ContactUs.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Contact from "../components/Contact"
-import MeetTheEBoard from "../components/MeetTheEboard";
+import Contact from "../components/contact/Contact"
+import MeetTheEBoard from "../components/contact/MeetTheEboard";
 
 export default function ContactUs() {
   return (
