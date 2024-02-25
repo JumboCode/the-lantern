f2f5b17c7d44fc652f4af284cac2074ce4bb7e22
tlai18 <thomas.lai@tufts.edu>
2024-02-24 23:49:09 -0500

    file drop down => added current image

--- lantern-club/src/components/magazine/FileDrop.js
+++ lantern-club/src/components/magazine/FileDrop.js
@@ -1,39 +1,53 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 
 const FileDrop = () => {
         const [fileList, setFileList] = useState([]);
+        const [currentImage, setCurrentImage] = useState('');
 
         useEffect(() => {
-        const fetchFileList = async () => {
-        try {
-                const response = await axios.get('/api/listFiles');
-                setFileList(response.data.urls);
-        } catch (error) {
-                console.error('Error fetching file list:', error);
-        }
-        };
+                const fetchFileList = async () => {
+                try {
+                        const response = await axios.get('/api/listFiles');
+                        setFileList(response.data.urls);
+                        if (response.data.urls.length > 0) {
+                                // Automatically set the first image as the current image
+                                setCurrentImage(response.data.urls[0]);
+                        }
+                } catch (error) {
+                        console.error('Error fetching file list:', error);
+                }
+                };
 
-        fetchFileList();
+
+                fetchFileList();
         }, []);
-        
+
+        const handleImageChange = (event) => {
+                setCurrentImage(event.target.value);
+        };
 
         return (
             <div class="dropdown">
-                <select>
+                <select onChange={handleImageChange} value={currentImage}>
                         {fileList.map((url, index) => {
                         // Extract file name from the URL
                         let fileName = url.substring(url.lastIndexOf('/') + 1);
                         fileName = fileName.substring(fileName.indexOf('_') + 1);
                         
                         return (
-                        <option key={index} >
-                        <a href={url} target="_blank">{fileName.replace(/\.[^/.]+$/, "")}</a>
-                        
+                        <option key={index} value={url}>
+                                {fileName.replace(/\.[^/.]+$/, "")}
                         </option>
                         );
                         })}
                 </select>
+                {currentImage && (
+                <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                    <Link href={currentImage}><img src={currentImage} alt="Selected" style={{ maxWidth: '100%', maxHeight: '400px', objectFit: 'contain' }} /></Link>
+                </div>
+                )}
           </div> 
           
         );
