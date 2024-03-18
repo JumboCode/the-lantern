7028717d9a7f9c40e332bce538ede10e49f06200
tlai18 <thomas.lai@tufts.edu>
2024-03-18 01:39:12 -0400

    profile add modal showing, but not backend integrated

--- lantern-club/src/components/contact/EboardOverlay.tsx
+++ lantern-club/src/components/contact/EboardOverlay.tsx
@@ -7,7 +7,7 @@ interface OverlayProps {
     isVisible: boolean,
     type: "Add" | "Edit",
     onClose: () => void,
-    profile: ProfileType
+    profile?: ProfileType
     
 }
 
@@ -15,7 +15,7 @@ interface OverlayProps {
 const EboardOverlay = ( {isVisible, onClose, type, profile}: OverlayProps ) => {
     if (!isVisible) return null; 
 
-    const [formData, setFormData] = useState({id: profile.id, name: profile.name, pronouns: profile.pronouns, title: profile.title, email: profile.email, major: profile.major});
+    const [formData, setFormData] = useState({id: profile?.id, name: profile?.name, pronouns: profile?.pronouns, title: profile?.title, email: profile?.email, major: profile?.major});
 
     const handleChange = (event: any) => {
         const { name, value } = event.target;
@@ -80,48 +80,13 @@ const EboardOverlay = ( {isVisible, onClose, type, profile}: OverlayProps ) => {
     
     };
 
-    if (type == "Add") {
+    if (type === "Add" || type === "Edit") {
         return (
             <div className="flex fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm justify-center items-center z-0">
-                <div className="w-[800px] flex flex-col orange-border border-4 max-h-screen rounded-3xl bg-white">
+                <div className="w-[800px] h-[600px] flex flex-col orange-border border-4 max-h-screen rounded-3xl bg-white">
                     <button className="text-xl place-self-end mr-5 mt-2" onClick={() => onClose()}>x</button>
                     <div className="px-5 flex flex-col overflow-y-scroll">
-                        <h2 className="mb-5 text-4xl font-coolvetica underline decoration-sky-500">{type}</h2>       
-                        <div>
-                            <h1 className="pt-2 font-nunito text-lg">Name</h1>
-                            <input className="mt-2 p-3 w-full border-contact-g1 border-2 rounded-lg outline-gc2 h-8 contact-input font-nunito text-lg" type="text" id="name" name="name" onChange={handleChange}></input>
-                        </div>
-                        <div>
-                            <h2 className="mt-5 font-nunito text-lg">Pronouns</h2>
-                            <input className="mt-2 p-3 w-full border-contact-g1 border-2 rounded-lg outline-gc2 h-8 contact-input font-nunito text-lg" type="text" id="pronouns" name="pronouns" onChange={handleChange}></input>
-                        </div>
-                        <div>
-                            <h2 className="mt-5 font-nunito text-lg">Title</h2>
-                            <input className="mt-2 p-3 w-full border-contact-g1 border-2 rounded-lg outline-gc2 h-8 contact-input font-nunito text-lg" type="text" id="title" name="title" onChange={handleChange}></input>
-                        </div>
-                        <div>
-                            <h2 className="mt-5 font-nunito text-lg">Email</h2>
-                            <input className="mt-2 p-3 w-full border-contact-g1 border-2 rounded-lg outline-gc2 h-8 contact-input font-nunito text-lg" type="text" id="email" name="email" onChange={handleChange}></input>
-                        </div>
-                        <div>
-                        <h2 className="mt-5 mb-1 font-nunito text-lg">Cover Photo</h2>
-                            <button className="bg-slate-200 hover:bg-slate-300 w-24 h-14 rounded-lg mt-2">+</button> 
-                        </div> 
-                        <div className="flex justify-center text-md space-x-7 py-5">
-                            <Buttonv2 text="Save" action={handleAdd} color="blue" width="w-40"/>
-                            <Buttonv2 text="Cancel" action={onClose} color="red" width="w-40" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    } else if (type == "Edit") {
-        return (
-            <div className="flex fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm justify-center items-center z-0">
-                <div className="w-[800px] flex flex-col orange-border border-4 max-h-screen rounded-3xl bg-white">
-                    <button className="text-xl place-self-end mr-5 mt-2" onClick={() => onClose()}>x</button>
-                    <div className="px-5 flex flex-col overflow-y-scroll">
-                        <h2 className="mb-5 text-4xl font-coolvetica underline decoration-sky-500">{type}</h2>       
+                        <h2 className="mb-5 text-4xl font-coolvetica underline decoration-sky-500">{type}</h2>
                         <div>
                             <h1 className="pt-2 font-nunito text-lg">Name</h1>
                             <input className="mt-2 p-3 w-full border-contact-g1 border-2 rounded-lg outline-gc2 h-8 contact-input font-nunito text-lg" type="text" id="name" name="name" value={formData.name} onChange={handleChange}></input>
@@ -140,13 +105,12 @@ const EboardOverlay = ( {isVisible, onClose, type, profile}: OverlayProps ) => {
                         </div>
                         <div>
                         <h2 className="mt-5 mb-1 font-nunito text-lg">Cover Photo</h2>
-                            <button className="bg-slate-200 hover:bg-slate-300 w-24 h-14 rounded-lg mt-2">
-                                +
-                            </button> 
-                        </div> 
+                            <button className="bg-slate-200 hover:bg-slate-300 w-24 h-14 rounded-lg mt-2">+</button> 
+                        </div>
                         <div className="flex justify-center text-md space-x-7 py-5">
-                            <Buttonv2 text="Save" action={handleEdit} color="blue" width="w-40"/>
-                            <Buttonv2 text="Delete" action={handleDelete} color="red" width="w-40" />
+                            <Buttonv2 text={type === "Add" ? "Save" : "Update"} action={type === "Add" ? handleAdd : handleEdit} color="blue" width="w-40"/>
+                            {type === "Edit" && <Buttonv2 text="Delete" action={handleDelete} color="red" width="w-40" />}
+                            {type === "Add" && <Buttonv2 text="Cancel" action={onClose} color="red" width="w-40" />}
                         </div>
                     </div>
                 </div>
--- lantern-club/src/components/contact/MeetTheEboard.tsx
+++ lantern-club/src/components/contact/MeetTheEboard.tsx
@@ -6,6 +6,8 @@ import Card from "./Card"
 import ProfileType from "@/types/profile";
 import EboardOverlay from "./EboardOverlay";
 import { useSession } from "next-auth/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 interface CardType {
     profile: ProfileType
@@ -41,7 +43,8 @@ const MeetTheEBoard = ({data}: MeetTheEBoardProps): JSX.Element => {
 
     const [showEditView, setShowEditView] = useState(false);
     const {data:session} = useSession();
-    const [showModal, setShowModal] = useState(false);
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
     const [currentCardData, setCurrentCardData] = useState<ProfileType>();
 
     const handleToggleEditView = () => {
@@ -50,20 +53,38 @@ const MeetTheEBoard = ({data}: MeetTheEBoardProps): JSX.Element => {
 
     const handleCardClick = (cardData: ProfileType) => {
         setCurrentCardData(cardData);
-        setShowModal(true);
+        setShowEditModal(true);
     };
+    
 
     return(
         <div>
             <div className="-mt-20 pt-32 w-full yellow-gradient">
                 <div className="ml-5">
 
-                    <h1 className="font-coolvetica text-7xl ml-12">Meet the E-Board
-                        { session?.user.isAdmin &&
-                        <text className="font-nunito underline text-2xl ml-7" onClick={handleToggleEditView}>edit</text>
-                        }
-                    </h1>
-                    
+                <h1 className="font-coolvetica text-7xl md:text-8xl ml-12 flex items-end w-11/12">
+                Meet the E-Board
+                {session?.user.isAdmin && (
+                    <span className="font-nunito underline text-2xl ml-7 cursor-pointer" onClick={handleToggleEditView}>
+                    edit
+                    </span>
+                )}
+                {showEditView && (
+                    <div className="flex items-center ml-auto">
+                        <FontAwesomeIcon onClick={() => setShowAddModal(true)} icon={faCirclePlus} className="text-7xl mr-2" /> {/* Adjust the icon size as necessary */}
+                        <span className="font-nunito text-lg">Add New</span>
+                        <div className="z-999">
+                            <EboardOverlay 
+                                type="Add" 
+                                isVisible={showAddModal} 
+                                onClose={() => {setShowAddModal(false)}} 
+                                profile={currentCardData}
+                            />
+                        </div>
+                    </div>
+                )}
+                </h1>
+
                 </div>
                 <div className="z-100" style={{ position: 'relative' }} >
                 <Carousel
@@ -88,11 +109,11 @@ const MeetTheEBoard = ({data}: MeetTheEBoardProps): JSX.Element => {
                         </div>
                     ))}
                 </Carousel>
-                {showModal && currentCardData && (
+                {showEditModal && currentCardData && (
                     <EboardOverlay 
                         type="Edit" 
-                        isVisible={showModal} 
-                        onClose={() => {setShowModal(false)}} 
+                        isVisible={showEditModal} 
+                        onClose={() => {setShowEditModal(false)}} 
                         profile={currentCardData}
                         />
                 )}
