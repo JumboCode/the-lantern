6ff040d32717c1603046e156e75d938465b3eb61
arjun <arjunkan2003@gmail.com>
2024-04-16 12:59:02 -0400

    cannot see file picture on my end

--- lantern-club/src/components/Navbar.tsx
+++ lantern-club/src/components/Navbar.tsx
@@ -1,32 +1,64 @@
-import React, { useState } from 'react'; // Corrected import of useState
+import React, { useState, useEffect } from 'react'; // Corrected import of useState
 import Link from "next/link";
 import Image from "next/image";
 import Hamburger from './Hamburger';
 
-const Navbar: React.FC = () => {
+function Navbar() {
+
+  const defaultLogo = "/images/thelantern1.png";
+  const toggleLogo = "/images/lantern-logo-blue.png";
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentLogo, setCurrentLogo] = useState(defaultLogo);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setCurrentLogo(toggleLogo);
+      } else {
+        setCurrentLogo(defaultLogo);
+      }
+    };
+
+    // Set the logo correctly on initial render
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const defaultLogo = "/images/thelantern1.png";
-  const toggleLogo = "/images/lanternlogo.PNG";
-
   return (
     <div className="w-screen">
-        <nav className="flex justify-between items-center text-lg font-kiona">
-          <div className="pl-10 md:pl-20 pr-10 md:pr-20 flex-shrink-0 pb-10">
-            <Link href="/" passHref>
-            <Image 
-              src={isMenuOpen ? toggleLogo : defaultLogo} 
-              height={200} 
-              width={200} 
-              alt="lantern logo" 
-              layout="intrinsic"
-            />
-            </Link>
-          </div>
+      <nav className="flex justify-between items-center text-lg font-kiona">
+        <div className="pl-10 md:pl-20 pr-10 md:pr-20 flex-shrink-0 pb-10">
+          <Link href="/" passHref>
+            <div>
+              <Image 
+                src={defaultLogo} 
+                height={200} 
+                width={200} 
+                alt="Default Logo" 
+                className="default-logo"
+                layout="intrinsic"
+              />
+              <Image 
+                src={toggleLogo} 
+                height={200} 
+                width={200} 
+                alt="Toggle Logo" 
+                className="toggle-logo"
+                layout="intrinsic"
+              />
+            </div>
+          </Link>
+        </div>
 
           <div className="md:hidden">
             <Hamburger onToggle={toggleMenu} />
