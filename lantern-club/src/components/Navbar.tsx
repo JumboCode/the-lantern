32ca282c5fbe1be98c6a94261fca8190eb18768f
Sristi Panchu <sristipanchu@gmail.com>
2024-04-23 02:44:01 -0400

    modular - nav links pulled from array

--- lantern-club/src/components/Navbar.tsx
+++ lantern-club/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 
 export default function NavBar() {
-  // State variable to track menu open/close
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Function to toggle menu state
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const links = [
+    { url: '/AboutUs', label: 'About Us' },
+    { url: '/Events', label: 'Events' },
+    { url: '/Magazine', label: 'Magazine' },
+    { url: '/Resources', label: 'Resources' },
+    { url: '/ContactUs', label: 'Contact Us' }
+  ];
 
   return (
     <div className="w-screen">
@@ -20,45 +21,14 @@ export default function NavBar() {
           </Link>
         </div>
 
-        {/* Hamburger menu icon */}
-        <div className="md:hidden flex items-center px-10">
-          <button onClick={toggleMenu}>
-            <div className="space-y-2">
-              <div className="h-0.5 w-8 bg-black"></div>
-              <div className="h-0.5 w-8 bg-black"></div>
-              <div className="h-0.5 w-8 bg-black"></div>
-            </div>
-          </button>
-        </div>
-
-
-        {/* Menu items */}
-        <ul className={`flex-col md:flex-row justify-center items-start md:justify-end space-y-4 md:space-y-0 md:space-x-8 pt-10 md:pt-0 overflow-x-auto pb-10 w-full ${isMenuOpen ? 'flex' : 'hidden md:flex'}`}>
-          <li>
-            <Link href="/AboutUs" className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
-              About Us
-            </Link>
-          </li>
-          <li>
-            <Link href="/Events" className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
-              Events
-            </Link>
-          </li>
-          <li>
-            <Link href="/Magazine" className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
-              Magazine
-            </Link>
-          </li>
-          <li>
-            <Link href="/Resources" className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
-              Resources
-            </Link>
-          </li>
-          <li>
-            <Link href="/ContactUs" className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
-              Contact Us
-            </Link>
-          </li>
+        <ul className="flex flex-row justify-center space-x-8 md:space-x-16 md:pr-20 pt-10 overflow-x-auto pb-10">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link href={link.url} className="hover:bg-nav-bg rounded-2xl p-3 relative transition-all duration-500">
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
