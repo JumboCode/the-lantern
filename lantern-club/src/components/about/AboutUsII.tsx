ca77ed227294d0542709873b2274b77bb9404231
21hwang <hannahwang714@gmail.com>
2024-04-23 22:55:29 -0400

    about us page styling

--- lantern-club/src/components/about/AboutUsII.tsx
+++ lantern-club/src/components/about/AboutUsII.tsx
@@ -29,7 +29,7 @@ export default function AboutUsII() {
         {/* First section */}
         
         <div className="orange mellow-yellow-border flex flex-col md:flex-row items-center" style={round_box}>
-          <div className="w-full md:w-1/3 rounded-3xl relative h-80 md:h-auto md:order-2">
+          <div className="w-full md:w-1/3 rounded-3xl relative h-80 md:h-auto md:order-2 md:pr-11">
             <Image
                 layout="responsive"
                 width={800}
@@ -39,8 +39,11 @@ export default function AboutUsII() {
                 alt="Lantern Club members at the Chat GPT meeting"
             />
           </div>
-          <div className="w-full md:w-2/3 font-nunito text-base md:text-lg md:order-1"> 
-            The Lantern is a <span className="font-bold">student-run, interdisciplinary think tank</span> that investigates the <span className="font-bold">social impact of science and technology</span> to advocate for responsible innovation. Our goal is to <span className="font-bold">educate the public</span> on issues like artificial intelligence, climate change tech, algorithms, ethical technology, <span className="font-bold">and more.</span>
+          <div className="w-full md:w-2/3 font-nunito text-base md:text-xl md:order-1 md:px-11"> 
+            <p className="leading-relaxed">
+            The Lantern is a <span className="font-nunitobold">student-run, interdisciplinary think tank</span> that investigates the <span className="font-nunitobold">social impact of science and technology</span> to advocate for responsible innovation. Our goal is to <span className="font-nunitobold">educate the public</span> on issues like artificial intelligence, climate change tech, algorithms, ethical technology, and more.
+            </p>
+            
           </div>
         </div>
         {/* Dotted line */}
@@ -49,7 +52,7 @@ export default function AboutUsII() {
 
         {/* Second section */}
         <div className="blue border-blue-100 flex flex-col md:flex-row items-center" style={round_box}>
-          <div className="w-full md:w-2/5 py-8 md:py-12 rounded-3xl relative h-80 md:h-auto md:order-1">
+          <div className="w-full md:w-2/5 py-8 md:py-12 rounded-3xl relative h-80 md:h-auto md:order-1 md:ml-11">
             <Image
                 layout="responsive"
                 width={800}
@@ -59,8 +62,8 @@ export default function AboutUsII() {
                 alt="Lantern Club members at the Chat GPT meeting"
             />
           </div>
-          <span className="text-white w-full md:w-3/5 font-nunito text-base md:text-lg md:order-2"> 
-            One of the primary objectives of The Lantern is to design and publish an <span className="font-extrabold">eponymous magazine</span> each academic year. Our <span className="font-extrabold">first issue</span> of The Lantern magazine was <span className="font-extrabold">published on May 11, 2023</span>, with written submissions <span className="font-bold">centering around science, technology, and society (STS) topics</span> from the Tufts University student body. We are excited to grow our community and <span className="font-bold">work towards publishing future issues.</span>
+          <span className="text-white w-full md:w-3/5 font-nunito text-base md:text-lg md:order-2 md:pr-11 md:pl-7 md:text-xl"> 
+            One of the primary objectives of The Lantern is to design and publish an <span className="font-nunitobold">eponymous magazine</span> each academic year. Our <span className="font-nunitobold">first issue</span> of The Lantern magazine was <span className="font-nunitobold">published on May 11, 2023</span>, with written submissions <span className="font-nunitobold">centering around science, technology, and society (STS) topics</span> from the Tufts University student body. We are excited to grow our community and <span className="font-nunitobold">work towards publishing future issues.</span>
             <br/><br/>
             <p className="font-black">You can read our first issue <Link href="/Magazine" target="_blank" rel="noopener noreferrer" className="underline">here.</Link></p>
           </span>
