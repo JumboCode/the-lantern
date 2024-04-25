a0ef19fb6d0dbbdfc086d79f9d7689669d7e5274
Thomas Lai <thomas.lai@tufts.edu>
2024-04-25 17:36:40 -0400

    suggested responsive version of header

--- lantern-club/src/components/home/HomepageI.tsx
+++ lantern-club/src/components/home/HomepageI.tsx
@@ -16,15 +16,15 @@ export default function HomepageI({title}:{title:string}){
                     <br></br> technology use and production</h1>
             </div>             */}
 
-                <div className="flex h-13 justify-center align-center bg-center bg-cover bg-no-repeat bg-[url('/images/background.png')]">
+                <div className="flex md:h-13 justify-center align-center bg-center bg-cover bg-no-repeat bg-[url('/images/background.png')] relative">
                   {/* <div className = "flex justify-between">
                     <img className="justify-self-start ml-20 h-lantern-pic" src={"/images/lantern.png"}/>
                     </div> */}
-                    <div className='flex flex-col col-1 '>
-                    <img className="flex text-center h-lantern-pic" src={"/images/text-logo.png"}/> 
-                    {/* <h1 className="justify-self-center mt-40 text-6xl md:text-8xl font-kiona font-bold text-center">The Lantern</h1> */}
-                    <h1 className="justify-self-center mt-20 text-2xl md:text-4xl font-nunitolight italic text-center tracking-wide">A think-tank advocating for socially responsible
-                    <br></br> technology use and production</h1>
+                    <div className='mx-5 flex flex-col col-1 '>
+                      <img className="flex text-center md:h-lantern-pic" src={"/images/text-logo.png"}/> 
+                      {/* <h1 className="justify-self-center mt-40 text-6xl md:text-8xl font-kiona font-bold text-center">The Lantern</h1> */}
+                      <h1 className="justify-self-center my-20 text-2xl md:text-4xl font-nunitolight italic text-center tracking-wide">A think-tank advocating for socially responsible
+                      <br/>technology use and production</h1>
                     </div>
                     {/* <img className="justify-self-end mr-20 h-lantern-pic" src={"/images/lantern.png"}/> */}
 
