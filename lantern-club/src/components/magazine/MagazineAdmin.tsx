12279b54d0444bc77092660c109f9b1b652b8113
Thomas Lai <thomas.lai@tufts.edu>
2024-04-24 22:07:10 -0400

    move delete file functionality in utils

--- lantern-club/src/components/magazine/MagazineAdmin.tsx
+++ lantern-club/src/components/magazine/MagazineAdmin.tsx
@@ -1,5 +1,4 @@
-import { Key, useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 
 import FileUpload from ".//FileUpload";
 import FileDrop from ".//FileDrop";
@@ -7,6 +6,7 @@ import Buttonv2 from "../Buttonv2";
 import ConfirmModal from "../ConfirmModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import FileDelete from '@/utils/FileDelete';
 // import extractFileKeyFromURL from '@/utils/extractFileKeyFromURL';
 
 export default function MagazineAdmin({ magazines }: { magazines: any[]}) {
@@ -17,96 +17,68 @@ export default function MagazineAdmin({ magazines }: { magazines: any[]}) {
         fontFamily: 'coolvetica',
         fontSize: '90px',
         lineHeight: '1',
-      };
-      const subheaderFont = {
-        fontFamily: 'nunito',
-        fontSize: '30px',
-        lineHeight: '1',
-        fontWeight: 'bold',
+  };
+  const subheaderFont = {
+    fontFamily: 'nunito',
+    fontSize: '30px',
+    lineHeight: '1',
+    fontWeight: 'bold',
 
-      };
+  };
+    
+
+  return (
+    <div className="-mt-20 p-20 pt-40 w-full yellow-gradient flex gap-5 flex-col">
+            <p style={headerFont}>Edit magazine issues</p>
+          <div className =" my-7">
+            <p style={subheaderFont}>Featured Issue</p>
+            <FileDrop />
+          </div>
+          
+          <div className = "mt-7"> 
+            <p style={subheaderFont}>Past Issues</p>
+          </div>
+
+          <ul>
+              {magazines.map((url: string, index: number) => {
+              // Extract file name from the URL
+              const keyName = "uploads/" + url.substring(url.lastIndexOf('/') + 1);
+              let fileName = keyName.substring(keyName.indexOf('_') + 1);
+              fileName = fileName.replace(/\.[^/.]+$/, "");
+
+              return (
+                  <li key={index} >
+                  
+                  {/* gets rid of the file extension */}
+                  <div className="flex pt-5 align-bottom">
+                  <a className="w-80 md:w-96 font-bold" href={url} target="_blank">{fileName}</a>
+                  <button onClick={() => setShowConfirmModal(keyName)
+                  }
+                  >                            
+                      <div className="svg-container hover:underline decoration-orange-400" >
+                    
+                      <FontAwesomeIcon
+                        icon={faTrashCan} 
+                        width={100}
+                        height={100}
+                        size='2x'
+                        className="cursor-pointer relative transition-all duration-300 hover:text-[#4279BC]"
+                      />
+
+                      </div>
+                      </button>
+                      <ConfirmModal isVisible={showConfirmModal === keyName} onClose={() => {setShowConfirmModal(null)}} onDelete={() =>  {FileDelete(keyName); setShowConfirmModal(null)}}/>
+                  </div>
+                  </li>
+              );
+              })}
+          </ul>
+
+          <FileUpload />
+          <Buttonv2 text="Save" action={() => console.log('okk>>')} color="blue" width="w-48" />
+          
       
-      const FileDelete = async (keyName: string) => {
-        try {
-          const response = await axios.delete(`/api/content/magazine`, { 
-            data: { keyName } 
-          });
-     
-          if (response.data.success) {
-
-            // console.log('File List before filtering:', fileList);
-            // console.log('Key Name to remove:', keyName);
-            // magazines.filter((fileUrl) => {
-            //     return extractFileKeyFromURL(fileUrl) !== keyName;
-            // });
-
-            
-            alert('File deleted successfully');
-          } else {
-            alert('Failed to delete the file');
-          }
-          setShowConfirmModal(null);
-        } catch (error) {
-          console.error('Error deleting the file:', error);
-          alert('Error deleting the file');
-          alert(keyName)
-
-        }
-      };
-
-        return (
-            <div className="-mt-20 p-20 pt-40 w-full yellow-gradient flex gap-5 flex-col">
-                    <p style={headerFont}>Edit magazine issues</p>
-                  <div className =" my-7">
-                    <p style={subheaderFont}>Featured Issue</p>
-                    <FileDrop />
-                  </div>
-                  
-                  <div className = "mt-7"> 
-                    <p style={subheaderFont}>Past Issues</p>
-                  </div>
-
-                  <ul>
-                      {magazines.map((url: string, index: number) => {
-                      // Extract file name from the URL
-                      const keyName = "uploads/" + url.substring(url.lastIndexOf('/') + 1);
-                      let fileName = keyName.substring(keyName.indexOf('_') + 1);
-                      fileName = fileName.replace(/\.[^/.]+$/, "");
-
-                      return (
-                          <li key={index} >
-                          
-                          {/* gets rid of the file extension */}
-                          <div className="flex pt-5 align-bottom">
-                          <a className="w-80 md:w-96 font-bold" href={url} target="_blank">{fileName}</a>
-                          <button onClick={() => setShowConfirmModal(keyName)
-                          }
-                          >                            
-                             <div className="svg-container hover:underline decoration-orange-400" >
-                            
-                              <FontAwesomeIcon
-                                icon={faTrashCan} 
-                                width={100}
-                                height={100}
-                                size='2x'
-                                className="cursor-pointer relative transition-all duration-300 hover:text-[#4279BC]"
-                              />
-
-                              </div>
-                              </button>
-                              <ConfirmModal isVisible={showConfirmModal === keyName} onClose={() => {setShowConfirmModal(null)}} onDelete={() => FileDelete(keyName)} />
-                          </div>
-                          </li>
-                      );
-                      })}
-                  </ul>
-
-                  <FileUpload />
-                  <Buttonv2 text="Save" action={() => console.log('okk>>')} color="blue" width="w-48" />
-                  
-              
-           </div>   
-            
-        );
+    </div>   
+);
 }
 
