3805dec10390ef341fc71625c57b67de93b00ee5
Sristi Panchu <sristipanchu@gmail.com>
2024-04-25 12:42:54 -0400

    bold+spacing fix

--- lantern-club/src/components/home/HomepageI.tsx
+++ lantern-club/src/components/home/HomepageI.tsx
@@ -21,8 +21,8 @@ export default function HomepageI({title}:{title:string}){
                     <img className="justify-self-start ml-20 h-lantern-pic" src={"/images/lantern.png"}/>
                     </div>
                     <div>
-                    <h1 className="justify-self-center mt-40 text-6xl md:text-8xl font-kiona text-center">The Lantern</h1>
-                    <h1 className="justify-self-center mt-10 text-2xl md:text-4xl font-nunitolight text-center tracking-wide">A think-tank advocating socially responsible
+                    <h1 className="justify-self-center mt-40 text-6xl md:text-8xl font-kiona font-bold text-center">The Lantern</h1>
+                    <h1 className="justify-self-center mt-20 text-2xl md:text-4xl font-nunitolight text-center tracking-wide">A think-tank advocating socially responsible
                     <br></br> technology use and production</h1>
                     </div>
                     <img className="justify-self-end mr-20 h-lantern-pic" src={"/images/lantern.png"}/>
