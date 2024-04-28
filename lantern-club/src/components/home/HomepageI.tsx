c686e4ac2cc76477bfde404a38e8214a5e8809b1
Sristi Panchu <sristipanchu@gmail.com>
2024-04-27 20:05:35 -0400

    content not responsive but pentagons r a vibe

--- lantern-club/src/components/home/HomepageI.tsx
+++ lantern-club/src/components/home/HomepageI.tsx
@@ -6,32 +6,16 @@ export default function HomepageI({title}:{title:string}){
       }; 
     return(
         <div className="drop-shadow-md">
-            {/* <div className="flex h-13 bg-center bg-cover bg-no-repeat bg-[url('/images/background.png')] grid grid-rows-3">
-                  <div className = "flex justify-between">
-                    <img className="justify-self-start ml-20" src={"/images/lantern.png"}/>
-            <div className="pb-20 flex h-13 bg-center bg-cover bg-no-repeat bg-[url('/images/background.png')] grid grid-rows-3" id="header-pent">
-                    <img className="justify-self-end mr-20" src={"/images/lantern.png"}/>
-                    </div>
-                    <h1 className="justify-self-center mb-20 text-6xl md:text-8xl font-kiona text-right">The Lantern</h1>
-                    <h1 className="justify-self-center text-2xl md:text-4xl font-nunitolight text-center tracking-wide">A think-tank advocating socially responsible
-                    <br></br> technology use and production</h1>
-            </div>             */}
 
                 <div className="flex justify-center align-center bg-center bg-cover bg-no-repeat bg-[url('/images/background.png')] relative" id="header-pent">
-                  {/* <div className = "flex justify-between">
-                    <img className="justify-self-start ml-20 h-lantern-pic" src={"/images/lantern.png"}/>
-                    </div> */}
                     <div className='mx-5 flex flex-col col-1 '>
                       <img className="flex text-center md:h-lantern-pic" src={"/images/text-logo.png"}/> 
                       {/* <h1 className="justify-self-center mt-40 text-6xl md:text-8xl font-kiona font-bold text-center">The Lantern</h1> */}
-                      <h1 className="justify-self-center my-20 text-2xl md:text-4xl font-nunitolight italic text-center tracking-wide">A think-tank advocating for socially responsible
+                      <h1 className="justify-self-center my-20 text-2xl pb-20 md:text-4xl font-nunitolight italic text-center tracking-wide">A think-tank advocating for socially responsible
                       <br/>technology use and production</h1>
                     </div>
-                    {/* <img className="justify-self-end mr-20 h-lantern-pic" src={"/images/lantern.png"}/> */}
 
             </div>  
-
-            {/* <div className="h-20 w-full bg-bottom bg-cover bg-no-repeat bg-[url('/images/background.png')]" id="triangle"></div> */}
         </div>
      )
 }
