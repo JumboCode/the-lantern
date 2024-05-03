387f9868f2cbd2c99fc2f6bb73fb017314366078
Sristi Panchu <sristipanchu@gmail.com>
2024-05-03 01:29:35 -0400

    header done

--- lantern-club/src/components/Header.tsx
+++ lantern-club/src/components/Header.tsx
@@ -9,11 +9,11 @@ export default function Header({title}:{title:string}){
         <div className="drop-shadow-md">
                 <div className="flex items-start justify-center bg-center pb-20 bg-cover bg-no-repeat bg-[url('/images/header-bg.png')]"  id="header2-pent">
                         <div className='flex flex-shrink-0'>
-                                <Image src={"/images/short-lantern.png"} height={90} width={90} alt="lantern logo" />
+                                <Image src={"/images/short-lantern.png"} height={77} width={77} alt="lantern logo" />
                         </div>
-                        <h1 className="flex text-4xl flex-shrink md:ml-16 md:mr-16 pt-24 md:text-8xl font-kiona text-center">{title}</h1>
+                        <h1 className="flex text-4xl flex-shrink md:ml-16 md:mr-16 pt-24 md:text-[5.2rem] font-kiona text-center">{title}</h1>
                         <div className='flex flex-shrink-0'>
-                                <Image src={"/images/short-lantern.png"} height={90} width={90}  alt="lantern logo" />
+                                <Image src={"/images/short-lantern.png"} height={77} width={77}  alt="lantern logo" />
                         </div>
                 </div>            
         </div>
